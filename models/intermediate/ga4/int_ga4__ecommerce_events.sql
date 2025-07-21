22cab70cdfba66d46aaf9b0c3f437648218d076b
Kyle Jamieson <krjamies@gmail.lcom>
2025-07-21 15:50:35 -0500

    Add initial SQL models for ecommerce events and user sessions

---
+++ models/intermediate/ga4/int_ga4__ecommerce_events.sql
@@ -0,0 +1,9 @@
+SELECT
+  user_pseudo_id,
+  event_name,
+  item_name,
+  quantity,
+  event_value,
+  event_timestamp
+FROM {{ ref('int_ga4__sessionized_events') }}
+WHERE event_name IN ('view_item', 'add_to_cart', 'purchase')
---
+++ models/intermediate/ga4/int_ga4__user_sessions.sql
@@ -0,0 +1,9 @@
+SELECT
+  user_pseudo_id,
+  MIN(event_timestamp) AS session_start,
+  MAX(event_timestamp) AS session_end,
+  COUNT(*) AS event_count,
+  COUNTIF(event_name = 'page_view') AS pageviews,
+  SUM(event_value) AS session_value
+FROM {{ ref('int_ga4__sessionized_events') }}
+GROUP BY user_pseudo_id, session_id
